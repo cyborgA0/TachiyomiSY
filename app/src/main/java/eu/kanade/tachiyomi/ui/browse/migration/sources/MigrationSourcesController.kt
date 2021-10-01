28cbe3d3649caadd3a0735bd32b0f963c7d54d6e
Andreas <andreas.everos@gmail.com>
2021-10-01 23:37:43 +0200

    Ability to order sources by library count when migrating  (#6000)
    * order sources by library count when migrating (closes #4703)
    
    * Use plain menu instead of full-on sheet

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/MigrationSourcesController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/MigrationSourcesController.kt
@@ -9,27 +9,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import dev.chrisbanes.insetter.applyInsetter
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.data.database.DatabaseHelper
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.databinding.MigrationSourcesControllerBinding
 import eu.kanade.tachiyomi.ui.base.controller.NucleusController
 import eu.kanade.tachiyomi.ui.base.controller.withFadeTransaction
-import eu.kanade.tachiyomi.ui.browse.BrowseController
-import eu.kanade.tachiyomi.ui.browse.migration.advanced.design.PreMigrationController
 import eu.kanade.tachiyomi.ui.browse.migration.manga.MigrationMangaController
-import eu.kanade.tachiyomi.util.lang.launchUI
-import eu.kanade.tachiyomi.util.lang.withUIContext
 import eu.kanade.tachiyomi.util.system.openInBrowser
-import exh.util.executeOnIO
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
+import uy.kohesive.injekt.injectLazy
 
 class MigrationSourcesController :
     NucleusController<MigrationSourcesControllerBinding, MigrationSourcesPresenter>(),
-    FlexibleAdapter.OnItemClickListener,
-    // SY -->
-    SourceAdapter.OnAllClickListener {
-    // SY <--
+    FlexibleAdapter.OnItemClickListener {
+
+    private val preferences: PreferencesHelper by injectLazy()
 
     private var adapter: SourceAdapter? = null
 
@@ -68,10 +60,29 @@ class MigrationSourcesController :
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+        when (val itemId = item.itemId) {
             R.id.action_source_migration_help -> activity?.openInBrowser(HELP_URL)
+            R.id.asc_alphabetical, R.id.desc_alphabetical -> {
+                setSortingDirection(SortSetting.ALPHABETICAL, itemId == R.id.asc_alphabetical)
+            }
+            R.id.asc_count, R.id.desc_count -> {
+                setSortingDirection(SortSetting.TOTAL, itemId == R.id.asc_count)
+            }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setSortingDirection(sortSetting: SortSetting, isAscending: Boolean) {
+        val direction = if (isAscending) {
+            DirectionSetting.ASCENDING
+        } else {
+            DirectionSetting.DESCENDING
+        }
+
+        preferences.migrationSortingDirection().set(direction)
+        preferences.migrationSortingMode().set(sortSetting)
+
+        presenter.requestSortUpdate()
     }
 
     fun setSources(sourcesWithManga: List<SourceItem>) {
@@ -85,49 +96,22 @@ class MigrationSourcesController :
         adapter?.updateDataSet(sourcesWithManga)
     }
 
-    // SY -->
-    override fun getTitle(): String? {
-        return resources?.getString(R.string.source_migration)
-    }
-
-    override fun onItemClick(view: View?, position: Int): Boolean {
+    override fun onItemClick(view: View, position: Int): Boolean {
         val item = adapter?.getItem(position) as? SourceItem ?: return false
         val controller = MigrationMangaController(item.source.id, item.source.name)
-        val parentController = parentController
-        if (parentController is BrowseController) {
-            parentController.router.pushController(controller.withFadeTransaction())
-        } else {
-            router.pushController(controller.withFadeTransaction())
-        }
-
+        parentController!!.router.pushController(controller.withFadeTransaction())
         return false
     }
 
-    override fun onAllClick(position: Int) {
-        val item = adapter?.getItem(position) as? SourceItem ?: return
-
-        launchUI {
-            val manga = Injekt.get<DatabaseHelper>().getFavoriteMangas().executeOnIO()
-            val sourceMangas = manga.asSequence().filter { it.source == item.source.id }.mapNotNull { it.id }.toList()
-            withUIContext {
-                PreMigrationController.navigateToMigration(
-                    Injekt.get<PreferencesHelper>().skipPreMigration().get(),
-                    run {
-                        val parentController = parentController
-                        if (parentController is BrowseController) {
-                            parentController.router
-                        } else {
-                            router
-                        }
-                    },
-                    sourceMangas
-                )
-            }
-        }
+    enum class DirectionSetting {
+        ASCENDING,
+        DESCENDING;
     }
-    // SY <--
 
-    companion object {
-        private const val HELP_URL = "https://tachiyomi.org/help/guides/source-migration/"
+    enum class SortSetting {
+        ALPHABETICAL,
+        TOTAL;
     }
 }
+
+private const val HELP_URL = "https://tachiyomi.org/help/guides/source-migration/"
